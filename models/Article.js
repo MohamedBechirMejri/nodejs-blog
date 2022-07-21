538c1d77caa26ebf0e39f61ab1c1ac6ef65efa04
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-21 05:58:11 +0100

    add articleschema

---
+++ models/Article.js
@@ -0,0 +1,53 @@
+const { Schema, model } = require("mongoose");
+
+const commentSchema = new Schema(
+  {
+    body: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const articleSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    comments: [commentSchema],
+  },
+  { timestamps: true }
+);
+
+module.exports = model("Article", articleSchema);
