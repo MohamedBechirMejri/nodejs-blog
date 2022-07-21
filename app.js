c63a4a4a0fadb00061db23353b304c76c06d489f
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-21 05:44:35 +0100

    setup mongoose

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 require("dotenv").config();
+
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
+const compression = require("compression");
+const helmet = require("helmet");
+const createError = require("http-errors");
+
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
 const app = express();
+
+app.use(helmet());
+app.use(compression());
 
 app.use(logger("dev"));
 app.use(express.json());
