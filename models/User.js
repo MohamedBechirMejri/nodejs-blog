0634795e9961f49554977eb203acc67fc497ac3d
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-21 06:08:42 +0100

    update user model

--- models/User.js
+++ models/User.js
@@ -1,52 +1,51 @@
 const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
-  },
-  middleName: {
-    type: String,
-  },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    validate: {
-      validator(v) {
-        return /^(([^<>()\\[\].,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-          v
-        );
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate: {
+        validator(v) {
+          return /^(([^<>()\\[\].,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+            v
+          );
+        },
       },
     },
-  },
-  password: {
-    type: String,
-    required: true,
-    minlength: 8,
-    maxlength: 64,
-    trim: true,
-    validate: {
-      validator(v) {
-        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
-          v
-        );
-      },
+    picture: {
+      type: String,
+      required: true,
+      default: "https://picsum.photos/200/300",
     },
+    role: {
+      type: String,
+      enum: ["admin", "user", "editor"],
+      default: "user",
+    },
+    bookmarks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Article",
+      },
+    ],
+    articles: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Article",
+      },
+    ],
   },
-  role: {
-    type: String,
-    enum: ["admin", "user"],
-    default: "user",
-  },
-  isActive: {
-    type: Boolean,
-    default: true,
-  },
-});
+  { timestamps: true }
+);
