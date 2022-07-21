13d983a17632361404f10e5ab6dc3baba45a0f6f
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-21 09:44:21 +0100

    add articles route

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ mongoose.connect(process.env.MONGODB_URI, {
   useUnifiedTopology: true,
 });
 
-
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
@@ -18,7 +17,8 @@ const helmet = require("helmet");
 const createError = require("http-errors");
 
 const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+// const usersRouter = require("./routes/users");
+const articlesRouter = require("./routes/articles");
 
 const app = express();
 
@@ -32,6 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/articles", articlesRouter);
+// app.use("/users", usersRouter);
 
 module.exports = app;
