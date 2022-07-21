7636cd784f78b48fc7c5686daca3f84ee6108629
MohamedBechirMejri <mohamed.bechir.mejri.dev@gmail.com>
2022-07-21 06:02:02 +0100

    add user schema

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  middleName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: {
+      validator(v) {
+        return /^(([^<>()\\[\].,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+          v
+        );
+      },
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    maxlength: 64,
+    trim: true,
+    validate: {
+      validator(v) {
+        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(
+          v
+        );
+      },
+    },
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+});
